44586895d52c49fa6480a0d74ae824bd864c536c
dannica ong <chunyunn@gmail.com>
2019-03-21 10:44:10 +0800

    Refactored Subjects in SystemTests to Flashcards

--- src/test/java/systemtests/AddCommandSystemTest.java
+++ src/test/java/systemtests/AddCommandSystemTest.java
@@ -21,13 +21,13 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-import static seedu.address.testutil.TypicalSubjects.ALICE;
-import static seedu.address.testutil.TypicalSubjects.AMY;
-import static seedu.address.testutil.TypicalSubjects.BOB;
-import static seedu.address.testutil.TypicalSubjects.CARL;
-import static seedu.address.testutil.TypicalSubjects.HOON;
-import static seedu.address.testutil.TypicalSubjects.IDA;
-import static seedu.address.testutil.TypicalSubjects.KEYWORD_MATCHING_MEIER;
+import static seedu.address.testutil.TypicalFlashcards.ALICE;
+import static seedu.address.testutil.TypicalFlashcards.AMY;
+import static seedu.address.testutil.TypicalFlashcards.BOB;
+import static seedu.address.testutil.TypicalFlashcards.CARL;
+import static seedu.address.testutil.TypicalFlashcards.HOON;
+import static seedu.address.testutil.TypicalFlashcards.IDA;
+import static seedu.address.testutil.TypicalFlashcards.KEYWORD_MATCHING_MEIER;
 
 import org.junit.Test;
 
@@ -37,14 +37,14 @@ import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.UndoCommand;
 import seedu.address.model.Model;
-import seedu.address.model.subject.Address;
-import seedu.address.model.subject.Email;
-import seedu.address.model.subject.Name;
-import seedu.address.model.subject.Phone;
-import seedu.address.model.subject.Subject;
+import seedu.address.model.flashcard.Address;
+import seedu.address.model.flashcard.Email;
+import seedu.address.model.flashcard.Flashcard;
+import seedu.address.model.flashcard.Name;
+import seedu.address.model.flashcard.Phone;
 import seedu.address.model.tag.Tag;
-import seedu.address.testutil.SubjectBuilder;
-import seedu.address.testutil.SubjectUtil;
+import seedu.address.testutil.FlashcardBuilder;
+import seedu.address.testutil.FlashcardUtil;
 
 public class AddCommandSystemTest extends AddressBookSystemTest {
 
@@ -54,10 +54,11 @@ public class AddCommandSystemTest extends AddressBookSystemTest {
 
         /* ------------------------ Perform add operations on the shown unfiltered list ----------------------------- */
 
-        /* Case: add a subject without tags to a non-empty address book, command with leading spaces and trailing spaces
+        /* Case: add a flashcard without tags to a non-empty address book, command with leading spaces
+         * and trailing spaces
          * -> added
          */
-        Subject toAdd = AMY;
+        Flashcard toAdd = AMY;
         String command = "   " + AddCommand.COMMAND_WORD + "  " + NAME_DESC_AMY + "  " + PHONE_DESC_AMY + " "
                 + EMAIL_DESC_AMY + "   " + ADDRESS_DESC_AMY + "   " + TAG_DESC_FRIEND + " ";
         assertCommandSuccess(command, toAdd);
@@ -69,72 +70,72 @@ public class AddCommandSystemTest extends AddressBookSystemTest {
 
         /* Case: redo adding Amy to the list -> Amy added again */
         command = RedoCommand.COMMAND_WORD;
-        model.addSubject(toAdd);
+        model.addFlashcard(toAdd);
         expectedResultMessage = RedoCommand.MESSAGE_SUCCESS;
         assertCommandSuccess(command, model, expectedResultMessage);
 
-        /* Case: add a subject with all fields same as another subject in the address book except name -> added */
-        toAdd = new SubjectBuilder(AMY).withName(VALID_NAME_BOB).build();
+        /* Case: add a flashcard with all fields same as another flashcard in the address book except name -> added */
+        toAdd = new FlashcardBuilder(AMY).withName(VALID_NAME_BOB).build();
         command = AddCommand.COMMAND_WORD + NAME_DESC_BOB + PHONE_DESC_AMY + EMAIL_DESC_AMY + ADDRESS_DESC_AMY
                 + TAG_DESC_FRIEND;
         assertCommandSuccess(command, toAdd);
 
-        /* Case: add a subject with all fields same as another subject in the address book except phone and email
+        /* Case: add a flashcard with all fields same as another flashcard in the address book except phone and email
          * -> added
          */
-        toAdd = new SubjectBuilder(AMY).withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB).build();
-        command = SubjectUtil.getAddCommand(toAdd);
+        toAdd = new FlashcardBuilder(AMY).withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB).build();
+        command = FlashcardUtil.getAddCommand(toAdd);
         assertCommandSuccess(command, toAdd);
 
         /* Case: add to empty address book -> added */
-        deleteAllSubjects();
+        deleteAllFlashcards();
         assertCommandSuccess(ALICE);
 
-        /* Case: add a subject with tags, command with parameters in random order -> added */
+        /* Case: add a flashcard with tags, command with parameters in random order -> added */
         toAdd = BOB;
         command = AddCommand.COMMAND_WORD + TAG_DESC_FRIEND + PHONE_DESC_BOB + ADDRESS_DESC_BOB + NAME_DESC_BOB
                 + TAG_DESC_HUSBAND + EMAIL_DESC_BOB;
         assertCommandSuccess(command, toAdd);
 
-        /* Case: add a subject, missing tags -> added */
+        /* Case: add a flashcard, missing tags -> added */
         assertCommandSuccess(HOON);
 
         /* -------------------------- Perform add operation on the shown filtered list ------------------------------ */
 
-        /* Case: filters the subject list before adding -> added */
-        showSubjectsWithName(KEYWORD_MATCHING_MEIER);
+        /* Case: filters the flashcard list before adding -> added */
+        showFlashcardsWithName(KEYWORD_MATCHING_MEIER);
         assertCommandSuccess(IDA);
 
-        /* --------------------- Perform add operation while a subject card is selected ------------------------ */
+        /* --------------------- Perform add operation while a flashcard card is selected ------------------------ */
 
-        /* Case: selects first card in the subject list, add a subject -> added, card selection remains unchanged */
-        selectSubject(Index.fromOneBased(1));
+        /* Case: selects first card in the flashcard list, add a flashcard -> added, card selection remains unchanged */
+        selectFlashcard(Index.fromOneBased(1));
         assertCommandSuccess(CARL);
 
         /* ----------------------------------- Perform invalid add operations --------------------------------------- */
 
-        /* Case: add a duplicate subject -> rejected */
-        command = SubjectUtil.getAddCommand(HOON);
-        assertCommandFailure(command, AddCommand.MESSAGE_DUPLICATE_SUBJECT);
+        /* Case: add a duplicate flashcard -> rejected */
+        command = FlashcardUtil.getAddCommand(HOON);
+        assertCommandFailure(command, AddCommand.MESSAGE_DUPLICATE_FLASHCARD);
 
-        /* Case: add a duplicate subject except with different phone -> rejected */
-        toAdd = new SubjectBuilder(HOON).withPhone(VALID_PHONE_BOB).build();
-        command = SubjectUtil.getAddCommand(toAdd);
-        assertCommandFailure(command, AddCommand.MESSAGE_DUPLICATE_SUBJECT);
+        /* Case: add a duplicate flashcard except with different phone -> rejected */
+        toAdd = new FlashcardBuilder(HOON).withPhone(VALID_PHONE_BOB).build();
+        command = FlashcardUtil.getAddCommand(toAdd);
+        assertCommandFailure(command, AddCommand.MESSAGE_DUPLICATE_FLASHCARD);
 
-        /* Case: add a duplicate subject except with different email -> rejected */
-        toAdd = new SubjectBuilder(HOON).withEmail(VALID_EMAIL_BOB).build();
-        command = SubjectUtil.getAddCommand(toAdd);
-        assertCommandFailure(command, AddCommand.MESSAGE_DUPLICATE_SUBJECT);
+        /* Case: add a duplicate flashcard except with different email -> rejected */
+        toAdd = new FlashcardBuilder(HOON).withEmail(VALID_EMAIL_BOB).build();
+        command = FlashcardUtil.getAddCommand(toAdd);
+        assertCommandFailure(command, AddCommand.MESSAGE_DUPLICATE_FLASHCARD);
 
-        /* Case: add a duplicate subject except with different address -> rejected */
-        toAdd = new SubjectBuilder(HOON).withAddress(VALID_ADDRESS_BOB).build();
-        command = SubjectUtil.getAddCommand(toAdd);
-        assertCommandFailure(command, AddCommand.MESSAGE_DUPLICATE_SUBJECT);
+        /* Case: add a duplicate flashcard except with different address -> rejected */
+        toAdd = new FlashcardBuilder(HOON).withAddress(VALID_ADDRESS_BOB).build();
+        command = FlashcardUtil.getAddCommand(toAdd);
+        assertCommandFailure(command, AddCommand.MESSAGE_DUPLICATE_FLASHCARD);
 
-        /* Case: add a duplicate subject except with different tags -> rejected */
-        command = SubjectUtil.getAddCommand(HOON) + " " + PREFIX_TAG.getPrefix() + "friends";
-        assertCommandFailure(command, AddCommand.MESSAGE_DUPLICATE_SUBJECT);
+        /* Case: add a duplicate flashcard except with different tags -> rejected */
+        command = FlashcardUtil.getAddCommand(HOON) + " " + PREFIX_TAG.getPrefix() + "friends";
+        assertCommandFailure(command, AddCommand.MESSAGE_DUPLICATE_FLASHCARD);
 
         /* Case: missing name -> rejected */
         command = AddCommand.COMMAND_WORD + PHONE_DESC_AMY + EMAIL_DESC_AMY + ADDRESS_DESC_AMY;
@@ -153,7 +154,7 @@ public class AddCommandSystemTest extends AddressBookSystemTest {
         assertCommandFailure(command, String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
 
         /* Case: invalid keyword -> rejected */
-        command = "adds " + SubjectUtil.getSubjectDetails(toAdd);
+        command = "adds " + FlashcardUtil.getFlashcardDetails(toAdd);
         assertCommandFailure(command, Messages.MESSAGE_UNKNOWN_COMMAND);
 
         /* Case: invalid name -> rejected */
@@ -184,7 +185,7 @@ public class AddCommandSystemTest extends AddressBookSystemTest {
      * 2. Command box has the default style class.<br>
      * 3. Result display box displays the success message of executing {@code AddCommand} with the details of
      * {@code toAdd}.<br>
-     * 4. {@code Storage} and {@code SubjectListPanel} equal to the corresponding components in
+     * 4. {@code Storage} and {@code FlashcardListPanel} equal to the corresponding components in
      * the current model added with {@code toAdd}.<br>
      * 5. Browser url and selected card remain unchanged.<br>
      * 6. Status bar's sync status changes.<br>
@@ -192,30 +193,30 @@ public class AddCommandSystemTest extends AddressBookSystemTest {
      * {@code AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
      * @see AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)
      */
-    private void assertCommandSuccess(Subject toAdd) {
-        assertCommandSuccess(SubjectUtil.getAddCommand(toAdd), toAdd);
+    private void assertCommandSuccess(Flashcard toAdd) {
+        assertCommandSuccess(FlashcardUtil.getAddCommand(toAdd), toAdd);
     }
 
     /**
-     * Performs the same verification as {@code assertCommandSuccess(Subject)}. Executes {@code command}
+     * Performs the same verification as {@code assertCommandSuccess(Flashcard)}. Executes {@code command}
      * instead.
-     * @see AddCommandSystemTest#assertCommandSuccess(Subject)
+     * @see AddCommandSystemTest#assertCommandSuccess(Flashcard)
      */
-    private void assertCommandSuccess(String command, Subject toAdd) {
+    private void assertCommandSuccess(String command, Flashcard toAdd) {
         Model expectedModel = getModel();
-        expectedModel.addSubject(toAdd);
+        expectedModel.addFlashcard(toAdd);
         String expectedResultMessage = String.format(AddCommand.MESSAGE_SUCCESS, toAdd);
 
         assertCommandSuccess(command, expectedModel, expectedResultMessage);
     }
 
     /**
-     * Performs the same verification as {@code assertCommandSuccess(String, Subject)} except asserts that
+     * Performs the same verification as {@code assertCommandSuccess(String, Flashcard)} except asserts that
      * the,<br>
      * 1. Result display box displays {@code expectedResultMessage}.<br>
-     * 2. {@code Storage} and {@code SubjectListPanel} equal to the corresponding components in
+     * 2. {@code Storage} and {@code FlashcardListPanel} equal to the corresponding components in
      * {@code expectedModel}.<br>
-     * @see AddCommandSystemTest#assertCommandSuccess(String, Subject)
+     * @see AddCommandSystemTest#assertCommandSuccess(String, Flashcard)
      */
     private void assertCommandSuccess(String command, Model expectedModel, String expectedResultMessage) {
         executeCommand(command);
@@ -229,7 +230,7 @@ public class AddCommandSystemTest extends AddressBookSystemTest {
      * 1. Command box displays {@code command}.<br>
      * 2. Command box has the error style class.<br>
      * 3. Result display box displays {@code expectedResultMessage}.<br>
-     * 4. {@code Storage} and {@code SubjectListPanel} remain unchanged.<br>
+     * 4. {@code Storage} and {@code FlashcardListPanel} remain unchanged.<br>
      * 5. Browser url, selected card and status bar remain unchanged.<br>
      * Verifications 1, 3 and 4 are performed by
      * {@code AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
