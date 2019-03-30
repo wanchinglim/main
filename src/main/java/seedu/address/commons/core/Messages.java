ae49e37be7f08cdcca7228896dd6f3c499ee49c5
jemine1998 <jemine1998@hotmail.com>
2019-03-30 14:55:11 +0800

    added select subject command

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -7,7 +7,8 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_FLASHCARD_DISPLAYED_INDEX = "The flashcard index provided is invalid";
+    public static final String MESSAGE_INVALID_FLASHCARD_DISPLAYED_INDEX = "The flashcard index provided is invalid!";
+    public static final String MESSAGE_INVALID_SUBJECT = "The subject provided is not found!";
     public static final String MESSAGE_FLASHCARDS_LISTED_OVERVIEW = "%1$d subjects listed!";
 
 }
---
+++ src/main/java/seedu/address/logic/commands/SelectSubjectCommand.java
@@ -0,0 +1,54 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.tag.SubjectTag;
+
+/**
+ * Selects a flashcard identified using it's displayed index from the flash book.
+ */
+public class SelectSubjectCommand extends Command {
+
+    public static final String COMMAND_WORD = "selectSubject";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Selects all flashcards with identified subject.\n"
+            + "Parameters: Subject (must be already in subject list panel)\n"
+            + "Example: " + COMMAND_WORD + " science";
+
+    public static final String MESSAGE_SELECT_SUBJECT_SUCCESS = "Selected Subject: %1$s";
+
+    private static SubjectTag targetSubject;
+
+    public SelectSubjectCommand(SubjectTag targetSubject) {
+        this.targetSubject = targetSubject;
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        requireNonNull(model);
+
+        List<SubjectTag> filteredSubjectBook = model.getFilteredSubjectList();
+
+        if (!filteredSubjectBook.contains(targetSubject)) {
+            throw new CommandException(Messages.MESSAGE_INVALID_SUBJECT);
+        }
+
+        model.setSelectedSubject(targetSubject);
+        return new CommandResult(String.format(MESSAGE_SELECT_SUBJECT_SUCCESS, targetSubject));
+
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof SelectSubjectCommand // instanceof handles nulls
+                && targetSubject.equals(((SelectSubjectCommand) other).targetSubject)); // state check
+    }
+}
