6862978ff4982eea9fa7e18fb6c46f242f90ae1c
jemine1998 <jemine1998@hotmail.com>
2019-03-30 12:22:50 +0800

    edited difficulty

--- src/main/java/seedu/address/model/flashcard/Difficulty.java
+++ src/main/java/seedu/address/model/flashcard/Difficulty.java
@@ -11,7 +11,7 @@ public class Difficulty {
 
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Difficulty levels should only contain numbers, and it should be at least 3 digits long";
+            "Difficulty levels should only contain number 1 (easy), 2 (medium) or 3 (difficult)!";
     public static final String VALIDATION_REGEX = "[1-3]";
     public final String value;
     public final Integer difficultyLevel;
