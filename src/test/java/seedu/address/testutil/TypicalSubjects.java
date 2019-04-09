290ce0f4efb9ca91d3bc1ab44f93d37769ced282
Wanching <holler.wanching@gmail.com>
2019-04-10 02:30:32 +0800

    Created TypicalSubjects in Test

---
+++ src/test/java/seedu/address/testutil/TypicalSubjects.java
@@ -0,0 +1,57 @@
+package seedu.address.testutil;
+
+import seedu.address.model.flashcard.Flashcard;
+import seedu.address.model.subject.SubjectBook;
+import seedu.address.model.tag.SubjectTag;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static seedu.address.logic.commands.CommandTestUtil.*;
+
+public class TypicalSubjects {
+
+    public static final SubjectTag ENGLISH = new SubjectTagBuilder().withTags("english").build();
+    public static final SubjectTag CHINESE = new SubjectTagBuilder().withTags("chinese").build();
+    public static final SubjectTag MATH = new SubjectTagBuilder().withTags("math").build();
+    public static final SubjectTag CHEMISTRY = new SubjectTagBuilder().withTags("chemistry").build();
+    public static final SubjectTag PHYSICS = new SubjectTagBuilder().withTags("physics").build();
+    public static final SubjectTag GEOGRAPHY = new SubjectTagBuilder().withTags("geography").build();
+    public static final SubjectTag HISTORY = new SubjectTagBuilder().withTags("history").build();
+
+
+    // Manually added
+    public static final Flashcard HOON = new FlashcardBuilder().withTopic("Hoon Meier").withDifficulty("3")
+            .withContent("little india").build();
+    public static final Flashcard IDA = new FlashcardBuilder().withTopic("Ida Mueller").withDifficulty("1")
+            .withContent("chicago ave").build();
+
+    // Manually added - Flashcard's details found in {@code CommandTestUtil}
+    public static final Flashcard AMY =
+            new FlashcardBuilder().withTopic(VALID_TOPIC_AMY).withDifficulty(VALID_DIFFICULTY_AMY)
+                    .withContent(VALID_CONTENT_AMY).withTags(VALID_TAG_FRIEND).build();
+    public static final Flashcard BOB =
+            new FlashcardBuilder().withTopic(VALID_TOPIC_BOB).withDifficulty(VALID_DIFFICULTY_BOB)
+                    .withContent(VALID_CONTENT_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
+                    .build();
+
+    public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
+
+    private TypicalSubjects() {} // prevents instantiation
+
+    /**
+     * Returns an {@code FlashBook} with all the typical subjects.
+     */
+    public static SubjectBook getTypicalSubjectBook() {
+        SubjectBook sb = new SubjectBook();
+        for (SubjectTag subjectTag : getTypicalSubjects()) {
+            sb.addSubject(subjectTag);
+        }
+        return sb;
+    }
+
+    public static List<SubjectTag> getTypicalSubjects() {
+        return new ArrayList<>(Arrays.asList(ENGLISH, CHINESE, MATH, CHEMISTRY, PHYSICS, GEOGRAPHY, HISTORY));
+    }
+}
