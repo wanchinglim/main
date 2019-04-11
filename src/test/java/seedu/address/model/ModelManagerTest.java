5f5c4a5c24ef9edb7b94459e88a9b81ff73680e2
Wanching <holler.wanching@gmail.com>
2019-04-11 22:44:19 +0800

    ModelManagerTest: Updated tests

--- src/test/java/seedu/address/model/ModelManagerTest.java
+++ src/test/java/seedu/address/model/ModelManagerTest.java
@@ -3,10 +3,9 @@ package seedu.address.model;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-//import static seedu.address.model.Model.PREDICATE_SHOW_ALL_FLASHCARDS;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_FLASHCARDS;
 import static seedu.address.testutil.TypicalFlashcards.CHINESE;
 import static seedu.address.testutil.TypicalFlashcards.ENGLISH;
-//import static seedu.address.testutil.TypicalFlashcards.MATH;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -19,11 +18,13 @@ import org.junit.rules.ExpectedException;
 
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.model.flashcard.Flashcard;
-//import seedu.address.model.flashcard.TopicContainsKeywordsPredicate;
+import seedu.address.model.flashcard.TopicContainsKeywordsPredicate;
 import seedu.address.model.flashcard.exceptions.FlashcardNotFoundException;
 import seedu.address.model.subject.SubjectBook;
+import seedu.address.model.tag.SubjectTag;
 import seedu.address.testutil.FlashBookBuilder;
 import seedu.address.testutil.FlashcardBuilder;
+import seedu.address.testutil.SubjectBookBuilder;
 
 public class ModelManagerTest {
     @Rule
@@ -153,14 +154,17 @@ public class ModelManagerTest {
     @Test
     public void equals() {
         FlashBook flashBook = new FlashBookBuilder().withFlashcard(ENGLISH).withFlashcard(CHINESE).build();
+        SubjectBook subjectBook = new SubjectBookBuilder()
+                .withSubject(new SubjectTag("english"))
+                .withSubject(new SubjectTag("chinese")).build();
         FlashBook differentFlashBook = new FlashBook();
         UserPrefs userPrefs = new UserPrefs();
         SubjectBook differentSubjectBook = new SubjectBook();
 
-        /*// same values -> returns true
-        modelManager = new ModelManager(flashBook, userPrefs);
-        ModelManager modelManagerCopy = new ModelManager(flashBook, userPrefs);
-        assertTrue(modelManager.equals(modelManagerCopy));*/
+        // same values -> returns true
+        modelManager = new ModelManager(differentSubjectBook, flashBook, userPrefs);
+        ModelManager modelManagerCopy = new ModelManager(differentSubjectBook, flashBook, userPrefs);
+        assertTrue(modelManager.equals(modelManagerCopy));
 
         // same object -> returns true
         assertTrue(modelManager.equals(modelManager));
@@ -171,13 +175,13 @@ public class ModelManagerTest {
         // different types -> returns false
         assertFalse(modelManager.equals(5));
 
-        /*// different flashBook -> returns false
-        assertFalse(modelManager.equals(new ModelManager(differentFlashBook, userPrefs)));
+        // different flashBook -> returns false
+        assertFalse(modelManager.equals(new ModelManager(differentSubjectBook, differentFlashBook, userPrefs)));
 
         // different filteredList -> returns false
-        String[] keywords = ALICE.getTopic().fullTopic.split("\\s+");
+        String[] keywords = ENGLISH.getTopic().fullTopic.split("\\s+");
         modelManager.updateFilteredFlashcardList(new TopicContainsKeywordsPredicate(Arrays.asList(keywords)));
-        assertFalse(modelManager.equals(new ModelManager(flashBook, userPrefs)));
+        assertFalse(modelManager.equals(new ModelManager(subjectBook, flashBook, userPrefs)));
 
         // resets modelManager to initial state for upcoming tests
         modelManager.updateFilteredFlashcardList(PREDICATE_SHOW_ALL_FLASHCARDS);
@@ -185,6 +189,6 @@ public class ModelManagerTest {
         // different userPrefs -> returns false
         UserPrefs differentUserPrefs = new UserPrefs();
         differentUserPrefs.setFlashBookFilePath(Paths.get("differentFilePath"));
-        assertFalse(modelManager.equals(new ModelManager(flashBook, differentUserPrefs)));*/
+        assertFalse(modelManager.equals(new ModelManager(subjectBook, flashBook, differentUserPrefs)));
     }
 }
